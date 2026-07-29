20a8cfd06b7faf77dda58bbcd52ef03011e3ef47
Shashi <shashikanth0312@gmail.com>
2026-07-29 10:28:50 +0530

    adding

--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -23,8 +23,14 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
   value = module.bastion.sg_id
 }
 
-resource "aws_ssm_parameter" "ansible_sg_id" {
-  name  = "/${var.project_name}-${var.environment}/ansible_sg_id"
+resource "aws_ssm_parameter" "vpn_sg_id" {
+  name  = "/${var.project_name}-${var.environment}/vpn_sg_id"
   type  = "String"
-  value = module.ansible.sg_id
+  value = module.ansible.vpn_sg_id
+}
+
+resource "aws_ssm_parameter" "app_alb_sg_id" {
+  name  = "/${var.project_name}-${var.environment}/app_alb_sg_id"
+  type  = "String"
+  value = module.ansible.app_alb.sg_id
 }
