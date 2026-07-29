7c901adfa47724eef5a4a8209d31e499de6a46e4
Shashi <shashikanth0312@gmail.com>
2026-07-29 10:44:12 +0530

    adding

---
+++ 04-openvpn/main.tf
@@ -0,0 +1,19 @@
+module "vpn" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "${var.project_name}-${var.environment}-vpn"
+
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+
+  # converting string list to list and get first elementelement(split("," , data.aws_ssm_parameter.public_subnet_id.value), 0)
+
+  subnet_id     = local.public_subnet_id
+  ami = data.aws_ami.ami_info.id
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-bastion"
+    }
+  )
+}
