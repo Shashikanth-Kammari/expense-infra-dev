2571bd04e1e610a57561f070519664345943f43a
Shashi <shashikanth0312@gmail.com>
2026-07-29 19:22:53 +0530

    adding

--- 05-app-alb/main.tf
+++ 05-app-alb/main.tf
@@ -32,3 +32,23 @@ resource "aws_lb_listener" "http" {
   
   }
 }
+
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "*.app-${var.environment}"
+      type    = "A"
+      allow_overwrite = true
+      alies = {
+        name = aws_lb.app_alb.dns_name
+        zone_name = aws_lb.app_alb.zone_id
+      }
+    }
+  ]
+}
