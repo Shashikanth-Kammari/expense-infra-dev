03ad4893f12382e497425e684642a0c537e056d7
Shashi <shashikanth0312@gmail.com>
2026-07-29 18:21:20 +0530

    adding

--- 05-app-alb/main.tf
+++ 05-app-alb/main.tf
@@ -1,9 +1,9 @@
-resource "aws_lb" "test" {
+resource "aws_lb" "app_alb" {
   name               = "${var.project_name}-${var.environment}-app-alb"
   internal           = true
   load_balancer_type = "application"
   security_groups    = [data.aws_ssm_parameter.app_alb_sg_id]
-  subnets            = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
+  subnets            = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 
   enable_deletion_protection = flase
 
@@ -13,4 +13,22 @@ resource "aws_lb" "test" {
       Name = "${var.project_name}-${var.environment}-app-alb"
     }
   )
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed-response {
+      content_type  = "443"
+      message_body  = "this is fised respose from app alb"
+      status_code   = "HTTP_301"
+    }
+  
+  }
 }
