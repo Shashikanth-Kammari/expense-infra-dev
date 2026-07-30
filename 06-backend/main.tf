c295c8527968cce66357a4112341de15b2577e33
Shashi <shashikanth0312@gmail.com>
2026-07-30 12:45:38 +0530

    adding

--- 06-backend/main.tf
+++ 06-backend/main.tf
@@ -42,3 +42,17 @@ resource "null_resource" "backend" {
       ]
     }
 }  
+
+resource "aws_ec2_instance_state" "backend" {
+  instance_id = aws_instance.backend.id
+  state       = "stopped"
+  #stop the server only after when null resource provisioning is completed
+  depends_on = [null_resource.backend]
+}
+
+
+resource "aws_ami_from_instance" "backend" {
+  name               = "${var.project_name}-${var.environment}-${var.common_tags.component}"
+  source_instance_id = module.backend.id
+  depends_on = [aws_ec2_instance_state.backend]
+}
