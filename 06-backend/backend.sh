ddc29cfa7f9bfe9e7d709b713f30437a7bfd71b7
Shashi <shashikanth0312@gmail.com>
2026-07-30 12:30:41 +0530

    adding

--- 06-backend/backend.sh
+++ 06-backend/backend.sh
@@ -3,5 +3,5 @@ component=$1
 environment=$2
 dnf install ansible -y
 pip3.9 install boto3 botocore 
-ansible-pull -U https://github.com/Shashikanth-Kammari/expense-ansible-rules-tf.git main.yaml -e 
+ansible-pull -i localhost, -U https://github.com/Shashikanth-Kammari/expense-ansible-rules-tf.git main.yaml -e 
 component=$component -e environment=$environment
