152e97e610b46b2d6047f5ff97a058d6720daeab
Shashi <shashikanth0312@gmail.com>
2026-07-30 23:07:27 +0530

    adding

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -164,6 +164,15 @@ resource "aws_security_group_rule" "frontend_web_alb" {
   security_group_id = module.frontend.sg_id
 }
 
+resource "aws_security_group_rule" "web_alb_public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.web_alb.sg_id
+}
+
 resource "aws_security_group_rule" "frontend_bastion" {
   type              = "ingress"
   from_port         = 22
