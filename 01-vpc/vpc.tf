827570bcaef205214505d326ba139484ec9be1a8
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-01 23:35:54 +0530

    adding

--- 01-vpc/vpc.tf
+++ 01-vpc/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source                = "../../terraform-aws-vpc"
+  ##source                = "../../terraform-aws-vpc"
+  source                = "git::git@github.com:Shashikanth-Kammari/terraform-aws-vpc.git"
   project_name          = var.project_name
   common_tags           = var.common_tags
   public_subnet_cidrs   = var.public_subnet_cidrs
