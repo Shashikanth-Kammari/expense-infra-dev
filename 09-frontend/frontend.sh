7af2f97e97c26c654a774b9866560da17dcf60ac
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-03 17:00:55 +0530

    adding

---
+++ 09-frontend/frontend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+component=$1
+environment=$2
+dnf install nginx -y
+pip3.9 install boto3 botocore 
+ansible-pull -i localhost, -U https://github.com/Shashikanth-Kammari/expense-ansible-rules-tf.git main.yaml -e 
+component=$component -e env=$environment
